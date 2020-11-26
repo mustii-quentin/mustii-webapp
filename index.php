9114cbfa6aef16579c92d4c142389af44188aac7
LePtiDev <75quentinguerrier@gmail.com>
2020-11-26 17:48:11 +0100

    init project & setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Accueil</title>
+	<link rel="stylesheet" href="src/css/global/global.css">
+	<link rel="stylesheet" href="./src/css/global/text.css">
+	<link rel="stylesheet" href="./src/css/pages/index.css">
+</head>
+<body>
+	<section class="welcome">
+		<header></header>
+	</section>
+</body>
+</html>
